21a58daa1ab5c20d2014fc7a28595525a0ced6cd
chris <christopherovando@gmail.com>
2025-11-29 17:13:28 -0800

    This commit is for the test that checks the toString
    it checks that the correct information is contained in the String the
    toString creates.

--- src/testingArena/ReportTest.java
+++ src/testingArena/ReportTest.java
@@ -4,12 +4,16 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import parkingGarage.Report;
+import parkingGarage.Ticket;
+import java.util.ArrayList;
+import java.util.List;
 
 
 
 
 class ReportTest {
 	
+
 	@Test 
 	
 	//this is testing if the constructor that takes a file actually works as intended when a missing file is passed in
@@ -24,6 +28,32 @@ class ReportTest {
 		    assertNull(myReport.creationDate());
 		    assertEquals(-1, myReport.getMonth());
 		    assertEquals(-1, myReport.getYear());
+		
+	}
+	
+	@Test
+	// I'm testing if the toString actually works if it contains the right information that we assigned in it
+	void testToString() {
+		
+		 Ticket ticket = new Ticket();
+		    
+		 
+		 ticket.setLicensePlate("NoWayWeHaveThisLicensePlate@123");
+		 ticket.setFee(5.0);
+		 ticket.setTicketPaid(true);
+
+		 List<Ticket> tickets = new ArrayList<>();
+		 tickets.add(ticket);
+
+		 Report myReport = new Report(7, tickets);
+
+		 String result = myReport.toString();
+
+		 assertTrue(result.contains("7,"));
+		 assertTrue(result.contains("NoWayWeHaveThisLicensePlate@123"));
+		 assertTrue(result.contains("5.0"));
+		 assertFalse(result.isEmpty());
+		
 		
 	}
 	
